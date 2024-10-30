a365eee68ade430aa6ccc359076ac8363ab0248e
JUN(AnhHT) <pj_taskaji-concrete@concrete-corp.com>
2024-10-30 07:33:46 +0700

    update code commit

--- tour/exercise-loops-and-functions/exercise-loops-and-functions.go
+++ tour/exercise-loops-and-functions/exercise-loops-and-functions.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
+	z := float64(1)
+	z -= (z*z - x) / (2 * z)
+	return z
 }
 
 func main() {
 	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(2) == math.Sqrt(2))
 }
